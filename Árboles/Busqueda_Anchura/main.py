e71229d0bbb428c43e77af4142b3304914bb6f01
Porfririo Damian <escamilla.huerta.porfirio@gmail.com>
2022-09-15 13:08:40 -0500

    Lectura de matrices simetricas mejorada, y eliminador de loops listo.

--- Árboles/Busqueda_Anchura/main.py
+++ Árboles/Busqueda_Anchura/main.py
@@ -24,15 +24,19 @@ class Vértice:
 
 def lee_matriz():
     """
-    Esta función lee una matriz de adyacencia.
+    Esta función crea una matriz de adyacencia de un grafo solicitando las entradas a_{i,j} con i >= j.
     :return: Matriz leída
     """
     n = int(input("Ingrese el número de vértices del grafo: "))
-    M = [[0] * n for i in range(n)]
+    M = [[-1] * n for i in range(n)]
     for i in range(n):
         for j in range(n):
             if i >= j:
-                M[i][j] = int(input("Ingrese la entrada [{0}][{1}]: ".format(str(i + 1), str(j + 1))))
+                if i == j:
+                    while M[i][j]%2 != 0:
+                        M[i][j] = int(input("Ingrese la entrada M[{0}][{1}] (Esta debe ser par): ".format(str(i + 1), str(j + 1))))
+                else:
+                    M[i][j] = int(input("Ingrese la entrada M[{0}][{1}]: ".format(str(i + 1), str(j + 1))))
                 if i > j:
                     M[j][i] = M[i][j]
     return M
@@ -52,12 +56,16 @@ def dibuja_matriz(M):
 
 
 def elimina_bucles(M):
-    for i in range(M):
-        for j in range(M[i]):
+    n=len(M)
+    for i in range(n):
+        for j in range(n):
             if i == j:
                 M[i][j] = 0
-    return M
 
 
 M = lee_matriz()
+print("Matriz leida")
+dibuja_matriz(M)
+elimina_bucles(M)
+print("Matriz sin loops")
 dibuja_matriz(M)
